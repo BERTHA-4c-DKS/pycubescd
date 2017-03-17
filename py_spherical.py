fcc690cab25d0b9d91281a21366c0a2b666c535c
Loriano Storchi <loriano@storchi.org>
2017-03-17 16:06:07 +0100

    ported also py_spherical

--- py_spherical.py
+++ py_spherical.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np 
-import cubes 
 import argparse
 import copy 
+
+import sys
+
+sys.path.append("./modules")
+import load_cube
 
 """
 Generalized the idea of the isodensity: spatial grid is assegned tyo fragment 1 or 2 
@@ -12,6 +16,11 @@ depending on their relative density value.
 parser = argparse.ArgumentParser()
 parser.add_argument("-f","--file", help="cube format file to perform CD", type=str)
 parser.add_argument("--verbosity", help="increase output verbosity",action="store_true")
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    exit(1)
+
 args = parser.parse_args()
 #print(args.file)
 #parser.print_help()
@@ -20,10 +29,9 @@ args = parser.parse_args()
 if args.verbosity:
    print("verbosity turned on")  
 
-
 print('Reading...' + args.file)
-totcube = cubes.cube()
-totcube.read(args.file)
+totcube = load_cube.cube()
+totcube.readfile(args.file)
 
 center = [0.0,0.0,2.876163]
 r = []
@@ -35,6 +43,4 @@ for rr in [0., 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2., 2.2, 2.4, 2.6, 2
     r.append(radius)
     rint.append(integral)
     print(rr, integral)
-
-sys.exit()
 
