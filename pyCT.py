c9ebd64a38eff3f8247b38ee5af2894aa22ff363
Leonardo Belpassi <leo.belpassi@gmail.com>
2017-04-13 14:06:27 +0200

    Density based descriptors for polarization within fragments are implemented. It needs to be checked with the original work (JCTC,12, 1236 (2016).

--- pyCT.py
+++ pyCT.py
@@ -11,6 +11,9 @@ import load_cube
 """
 Generalized the idea of the isodensity: spatial grid is assegned tyo fragment 1 or 2 
 depending on their relative density value. 
+
+Density based descriptors for the CT and polarization effects associated with the interaction between two fragments and the formation of the chemical bond are reported.
+see "Advances in Charge Displacement Analysis" Bistoni, Belpassi, Taratelli JCTC, 12,1236-1244 (2016).
 """
 
 parser = argparse.ArgumentParser()
@@ -47,15 +50,70 @@ for i in range(len(totcube.get_rawdata())) :
       if (frag1cube.get_rawdata()[i] > frag2cube.get_rawdata()[i]):    
          partsum += totcube.get_rawdata()[i]
 
-print('Electronic Charge on frag1', partsum*dv)
+CTfrag1=partsum*dv
+
+print('Electronic Charge on frag1', CTfrag1)
 
 partsum = 0.
 for i in range(len(totcube.get_rawdata())) :
       if (frag1cube.get_rawdata()[i] < frag2cube.get_rawdata()[i]):    
          partsum += totcube.get_rawdata()[i]
 
-print('Electronic Charge on frag2', partsum*dv)
-
+CTfrag2=partsum*dv
+print('Electronic Charge on frag2', CTfrag2)
 print('Expected error due to cube sampling', totcube.integrate())
+
+
+partsum = 0.
+for i in range(len(totcube.get_rawdata())) :
+      if (totcube.get_rawdata()[i] < 0.0000):
+            partsum += totcube.get_rawdata()[i]
+
+Qtot = -partsum*dv
+
+if (CTfrag1 < 0.00 ) :
+     partsum = 0.
+     for i in range(len(totcube.get_rawdata())) :
+        if (totcube.get_rawdata()[i] > 0.0000):
+            if (frag1cube.get_rawdata()[i] > frag2cube.get_rawdata()[i]):
+               partsum += totcube.get_rawdata()[i]
+     Qfrag1 = partsum*dv
+
+     partsum = 0.
+     for i in range(len(totcube.get_rawdata())) :
+        if (totcube.get_rawdata()[i] < 0.0000):
+            if (frag1cube.get_rawdata()[i] < frag2cube.get_rawdata()[i]):
+               partsum += totcube.get_rawdata()[i]
+     Qfrag2 = -partsum*dv
+
+if (CTfrag1 > 0.00 ) :
+     partsum = 0.
+     for i in range(len(totcube.get_rawdata())) :
+        if (totcube.get_rawdata()[i] < 0.0000):
+            if (frag1cube.get_rawdata()[i] > frag2cube.get_rawdata()[i]):
+               partsum += totcube.get_rawdata()[i]
+     Qfrag1 = -partsum*dv
+
+     partsum = 0.
+     for i in range(len(totcube.get_rawdata())) :
+        if (totcube.get_rawdata()[i] > 0.0000):
+            if (frag1cube.get_rawdata()[i] < frag2cube.get_rawdata()[i]):
+               partsum += totcube.get_rawdata()[i]
+     Qfrag2 = partsum*dv
+
+
+print('')
+print('Electronic Charge on frag1, CT = ', CTfrag1)
+print('Note, a positive value means a charge accumulation in frag1')
+print('')
+print('Density based descriptors for CT and polarization:details see JCTC,12, 1236 (2016)')
+print('Qtot = Qfrag1+Qfrag2+|CT|')
+print('')
+print("Qtot  = %f " % Qtot)
+print("Qfrag1= %f " % Qfrag1)
+print("Qfrag2= %f " % Qfrag2)
+
+
+   
 
 sys.exit()
