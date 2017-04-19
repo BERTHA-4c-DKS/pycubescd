88c928c306fbf43596e2c1463ead90d3d6733edb
Loriano Storchi <loriano@storchi.org>
2017-04-19 15:17:31 +0200

    refactoring pyct.py

--- README.md
+++ README.md
@@ -15,6 +15,11 @@ pycdfrags.py: Produce a CD curve using three cubes the two fragments
 
 pycdexct.py: can be used for instance in the case of excited states 
 
+
+pydt.py: Generalized the idea of the isodensity: spatial grid is assegned tyo fragment 1 or 2 
+         depending on their relative density value. Density based descriptors for the CT and 
+         polarization effects associated with the interaction between two fragments and the formation
+         of the chemical bond are reported.
+
 pyDens_iso.py
 anche volendo py_spherical.py
-pyCT
--- pyct.py
+++ pyct.py
@@ -3,6 +3,7 @@ import numpy as np
 import argparse
 import copy 
 import sys
+import os
 
 sys.path.append("./modules")
 import load_cube
@@ -11,14 +12,16 @@ import load_cube
 """
 Generalized the idea of the isodensity: spatial grid is assegned tyo fragment 1 or 2 
 depending on their relative density value. 
-
-Density based descriptors for the CT and polarization effects associated with the interaction between two fragments and the formation of the chemical bond are reported.
+Density based descriptors for the CT and polarization effects associated with the 
+interaction between two fragments and the formation of the chemical bond are reported.
 see "Advances in Charge Displacement Analysis" Bistoni, Belpassi, Taratelli JCTC, 12,1236-1244 (2016).
 """
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f","--file", help="3 cube format files: tot frag1 frag2", type=str, nargs=3)
-parser.add_argument("--verbosity", help="increase output verbosity",action="store_true")
+parser.add_argument("-f","--file", help="3 cube format files: tot frag1 frag2 " \
+        " filename separator is a space", required=True, type=str, nargs=3)
+parser.add_argument("-v", "--verbose", help="increase output verbosity", default=False, \
+        action="store_true")
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -26,22 +29,41 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-if args.verbosity:
+if args.verbose:
    print("verbosity turned on")  
 
-print('Reading...' + args.file[0])
+if args.verbose:
+  print('Reading...' + args.file[0])
+
+if not (os.path.isfile(args.file[0])):
+    print "File ", args.file[0], " does not exist "
+    exit(1)
+
 totcube = load_cube.cube()
 totcube.readfile(args.file[0])
 
-print('Reading...' + args.file[1])
+if args.verbose:
+  print('Reading...' + args.file[1])
+
+if not (os.path.isfile(args.file[1])):
+    print "File ", args.file[1], " does not exist "
+    exit(1)
+
 frag1cube = load_cube.cube()
 frag1cube.readfile(args.file[1])
 
-print('Reading...' + args.file[2])
+if args.verbose:
+  print('Reading...' + args.file[2])
+
+if not (os.path.isfile(args.file[2])):
+    print "File ", args.file[2], " does not exist "
+    exit(1)
+
 frag2cube = load_cube.cube()
 frag2cube.readfile(args.file[2])
 
-print('Coping temp data...')
+if args.verbose:
+  print('Coping temp data...')
 
 dv = totcube.get_dx()*totcube.get_dy()*totcube.get_dz()
 
@@ -62,7 +84,6 @@ for i in range(len(totcube.get_rawdata())) :
 CTfrag2=partsum*dv
 print('Electronic Charge on frag2', CTfrag2)
 print('Expected error due to cube sampling', totcube.integrate())
-
 
 partsum = 0.
 for i in range(len(totcube.get_rawdata())) :
@@ -112,8 +133,3 @@ print('')
 print("Qtot  = %f " % Qtot)
 print("Qfrag1= %f " % Qfrag1)
 print("Qfrag2= %f " % Qfrag2)
-
-
-   
-
-sys.exit()
